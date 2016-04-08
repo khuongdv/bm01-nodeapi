42b03fb62007aaabf0a2c8ae3e5250da17cb9867
Đào Khương <minh.khuong1306@gmail.com>
2016-04-08 09:16:15 +0700

    Added files via upload

---
+++ scheduler/taskScheduler.js
@@ -0,0 +1,45 @@
+﻿'use strict';
+var staffService, dataUtils;
+/**
+ * arg0: staffService
+ * arg1: dataUtils 
+ * 
+ */
+module.exports.init = function(arg0, arg1) {
+    staffService = arg0;
+    dataUtils = arg1;
+}
+
+
+
+module.exports.getPasswordExpirationScheduler = function() {
+    var CronJob = require('cron').CronJob;
+    var job = new CronJob({
+        cronTime: '00 30 11 * * *',
+        onTick: function () {
+            doCheckAndSendEmail();
+        },
+        start: false       
+    });
+    return job;
+}
+
+//  Browse all staffs to check if his password is gonna be expired. If so, send email to ask him to change psw
+function doCheckAndSendEmail(){
+    var listExpired = staffService.getListStaffExpiredPsw();
+    if (dataUtils.isArrayEmpty(listExpired)) {
+        return;
+    }
+    // bat dau gui mail
+    for (var obj in arr) {
+        if (obj != undefined && obj != null && typeof obj == 'object') {
+            var staffName = obj.name;
+            var email = obj.email;
+            requestChangePassword(staffName, email);
+        }
+    }
+}
+// Send email for requesting change passowrd of staffName
+function requestChangePassword(staffName, email){
+    // Send email to staffName
+}
