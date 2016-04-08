c81268c83e314e852a7dc13752e14f9026bb8458
Đào Khương <minh.khuong1306@gmail.com>
2016-04-08 09:18:15 +0700

    Update encryptUtils.js

--- utils/encryptUtils.js
+++ utils/encryptUtils.js
@@ -1,10 +1,11 @@
 const SHA_SEED = "!@#FASFSA#DAGD$ADGAxzc_(*FA";
 var crypto = null;
+var Q = require("q");
 /**
 Create MD5 hash from the input
 Use this method to encode password before storing to DB
 */
-function getMD5Hash(pass){
+module.exports.getMD5Hash = function (pass){
 	if(crypto == null){
 		 crypto = require('crypto');
 	}
@@ -15,7 +16,8 @@ function getMD5Hash(pass){
 Use this method to generate token to send back to client. Beside, save this token to DB
 Every request from client that have valid token will be accepted.
 */
-function generateToken(staffCode){
+module.exports.generateToken = function (staffCode){
+	console.log("[EncryptUtil] - generateToken for " + staffCode);
 	if(crypto == null){
 		 crypto = require('crypto');
 	}
@@ -28,18 +30,17 @@ function generateToken(staffCode){
 /**
 Decrypt token to check its validity
 */
-function decryptToken(token){
+module.exports.decryptToken = function (token){
+	console.log("[EncryptUtil] - Decrypt token : " + token);
 	if(crypto == null){
 		 crypto = require('crypto');
 	}
 	var algorithm = 'aes256';		
-	var decipher = crypto.createDecipher(algorithm, SHA_SEED);
-	console.log("Token = " + token);
+	var decipher = crypto.createDecipher(algorithm, SHA_SEED);	
 	var decryptedText = decipher.update(token, 'hex', 'utf8') + decipher.final('utf8');
-	console.log("Decrypted text: " + decryptedText + "; type = " + typeof(decryptedText));
+	console.log("Decrypted text: " + decryptedText);
 	try{
-		var loginObject = JSON.parse(decryptedText);
-		console.log("Login object: "  + loginObject);
+		var loginObject = JSON.parse(decryptedText);		
 		if(loginObject.staffCode != undefined && loginObject.staffCode != null){
 			loginObject.Status = true;
 			return loginObject;
@@ -56,21 +57,20 @@ function decryptToken(token){
 	}
 }
 //--- Check if request has valid token
-function isAuthenticated(req){
+module.exports.isAuthenticated = function (req, StaffModel){
+	console.log("[EncryptUtil] - Check if the request is authenticated");
 	var token = req.get('token');
 	if(token == undefined || token == null) {
 		return false;
 	}
-	var extractedData = decryptToken(token);
-	console.log(extractedData);
-	if(typeof(extractedData) == "object" && extractedData.Status == true){
+	var extractedData = decryptToken(token);	
+    if (typeof (extractedData) == "object" && extractedData.Status == true) {
+        // Now check the token and staffCode if co-exist together
+        var staffCode = extractedData.staffCode;
+        StaffModel.findOne({ 'token': token, 'code': staffCode }, function (err, data) {
+            // PENDING
+        })
 		return true;
 	}
 	return false;
 }
-
-// Exports
-exports.getMD5Hash = getMD5Hash;
-exports.generateToken = generateToken;
-exports.decryptToken = decryptToken;
-exports.isAuthenticated = isAuthenticated;
