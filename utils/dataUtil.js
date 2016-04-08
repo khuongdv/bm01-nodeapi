e9404a410b78068fd42c58996bacbf6a09a079ee
Đào Khương <minh.khuong1306@gmail.com>
2016-04-08 09:17:50 +0700

    Added files via upload

---
+++ utils/dataUtil.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+module.exports.isEmpty = function (str) {
+    return str != undefined && str != null && str.trim() != '';
+}
+
+module.exports.isArrayEmpty = function (arr) {
+    if (arr === undefined || arr === null) {
+        return true;
+    }
+    if (!(arr instanceof Array) || arr.length == 0) {
+        return true;
+    }
+    return false;
+}
---
+++ utils/mongoModels.js
@@ -0,0 +1,30 @@
+﻿module.exports.getSchema = function (Schema, collection){
+    switch (collection) {
+        case "Staff":
+            return new Schema({
+                "name": String,
+                "code": String,
+                "birth": Date,
+                "gender": String,
+                "password": String,
+                "date_start": Date,
+                "status": String,
+                "token": String,
+                "token_created_at": Date
+            }, { collection : 'staff' });
+            break;
+        case "Category":
+            return new Schema({
+                "name": String,
+                "code": String,
+                "update_datetime": Date,
+                "status": String
+            }, { collection : 'category' });
+            break;
+        // and so on
+    }
+}
+
+module.exports.getModel = function (mongoose, objSchema, customName) {
+       return mongoose.model(customName, objSchema);
+}
